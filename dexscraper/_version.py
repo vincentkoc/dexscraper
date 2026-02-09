f497ab32360409765c690dbabf9f3945ecf3d183
Vincent Koc <vincentkoc@ieee.org>
2026-02-08 20:02:09 -0800

    fix: version in package

--- dexscraper/_version.py
+++ dexscraper/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev45+g845c5b3c6.d20250902'
-__version_tuple__ = version_tuple = (0, 1, 'dev45', 'g845c5b3c6.d20250902')
+__version__ = version = '0.1.3.dev13'
+__version_tuple__ = version_tuple = (0, 1, 3, 'dev13')
 
-__commit_id__ = commit_id = 'g845c5b3c6'
+__commit_id__ = commit_id = 'g35e37839a'
